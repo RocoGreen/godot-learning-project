b95c145b937be8c1c18ef51a2524a41af50e700b
Ethan FOKOU NGOUNOU <f.ethan@outlook.fr>
2026-08-17 15:45:22 +0200

    Updated PlayableCameraComponent's script
    Updated the component's script readability and it now reflects the transition from entity to playable component.

--- entities/playables/_shared/components/camera/playable_camera_component.gd
+++ entities/playables/_shared/components/camera/playable_camera_component.gd
@@ -5,9 +5,9 @@ extends Marker3D
 
 
 @export_group("Dependencies")
-@export var entity: PhysicsBody3D:
-	set(new_entity):
-		entity = new_entity;
+@export var playable: PhysicsBody3D:
+	set(new_playable):
+		playable = new_playable;
 
 		if Engine.is_editor_hint():
 			update_configuration_warnings();
@@ -22,10 +22,10 @@ extends Marker3D
 func _ready() -> void:
 	if Engine.is_editor_hint(): return;
 
-	AssertLib.assert_if_entity_not_found(entity);
+	AssertLib.assert_if_playable_not_found(playable);
 
 	_camera_pivot.set_as_top_level(true);
-	_camera_pivot.add_excluded_object(entity.get_rid());
+	_camera_pivot.add_excluded_object(playable.get_rid());
 
 
 func _physics_process(_delta: float) -> void:
@@ -38,71 +38,70 @@ func _physics_process(_delta: float) -> void:
 
 
 func _get_configuration_warnings() -> PackedStringArray:
-	return ConfigurationWarningLib.get_for_entity(entity);
+	return ConfigurationWarningLib.get_for_playable(playable);
 
 
 ## Note: The returned data (`Dictionary`) containing the results of the intersected ray 
 ## is the same as `PhysicsDirectSpaceState3D.intersect_ray()` method.
 func ray_from_camera_forward(
 		ray_collision_mask: int = CollisionMaskLib.get_entities_and_obstacles(),
-		exclude_entity_from_ray: bool = true,
+		exclude_playable_from_ray: bool = true,
 		ray_exclusions: Array[RID] = [],
 ) -> Dictionary:
 	var space_state: PhysicsDirectSpaceState3D = get_world_3d().get_direct_space_state();
 
 	var ray_start: Vector3 = _camera.get_global_position();
-	var camera_forward_direction: Vector3 = -_camera.global_basis.z.normalized();
-	var ray_end: Vector3 = ray_start + (camera_forward_direction * 1000.0);
+	var ray_end: Vector3 = get_virtual_camera_aim_point();
 
 	var ray_query := PhysicsRayQueryParameters3D.create(ray_start, ray_end);
 
-	if exclude_entity_from_ray:
-		ray_exclusions.append(entity.get_rid());
+	if exclude_playable_from_ray:
+		ray_exclusions.append(playable.get_rid());
 
 	ray_query.set_exclude(ray_exclusions);
 	ray_query.set_collision_mask(ray_collision_mask);
 
-	var ray_result: Dictionary = space_state.intersect_ray(ray_query);
+	var ray_results: Dictionary = space_state.intersect_ray(ray_query);
 
-	return ray_result;
+	return ray_results;
 
 
 func get_camera_aim_point_by_ray_else_virtual(
 		ray_collision_mask: int = CollisionMaskLib.get_entities_and_obstacles(),
-		exclude_entity_from_ray: bool = true,
+		exclude_playable_from_ray: bool = true,
 		ray_exclusions: Array[RID] = [],
 ) -> Vector3:
-	var ray_from_camera_forward_result: Dictionary = ray_from_camera_forward(
+	var camera_aim_point_by_ray: Vector3 = get_camera_aim_point_by_ray(
 			ray_collision_mask,
-			exclude_entity_from_ray,
+			exclude_playable_from_ray,
 			ray_exclusions,
 	);
 
-	var camera_aim_point: Vector3;
+	var final_camera_aim_point: Vector3;
 
-	if ray_from_camera_forward_result.has("position"):
-		camera_aim_point = ray_from_camera_forward_result.get("position");
+	if camera_aim_point_by_ray:
+		final_camera_aim_point = camera_aim_point_by_ray;
 	else:
-		camera_aim_point = get_virtual_camera_aim_point();
+		final_camera_aim_point = get_virtual_camera_aim_point();
 
-	return camera_aim_point;
+	return final_camera_aim_point;
 
 
 func get_camera_aim_point_by_ray(
 		ray_collision_mask: int = CollisionMaskLib.get_entities_and_obstacles(),
-		exclude_entity_from_ray: bool = true,
+		exclude_playable_from_ray: bool = true,
 		ray_exclusions: Array[RID] = [],
 ) -> Vector3:
-	var ray_from_camera_forward_result: Dictionary = ray_from_camera_forward(
+	var ray_from_camera_forward_results: Dictionary = ray_from_camera_forward(
 			ray_collision_mask,
-			exclude_entity_from_ray,
+			exclude_playable_from_ray,
 			ray_exclusions,
 	);
 
 	var camera_aim_point: Vector3 = Vector3.ZERO;
-	
-	if ray_from_camera_forward_result.has("position"):
-		camera_aim_point = ray_from_camera_forward_result.get("position");
+
+	if ray_from_camera_forward_results.has("position"):
+		camera_aim_point = ray_from_camera_forward_results.get("position");
 
 	return camera_aim_point;
 

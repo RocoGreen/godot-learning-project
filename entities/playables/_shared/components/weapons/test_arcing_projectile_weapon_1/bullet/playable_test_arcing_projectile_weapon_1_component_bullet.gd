a53ecb41e83bd45aa27d3470d102b00ff1227ac0
Ethan FOKOU NGOUNOU <f.ethan@outlook.fr>
2026-09-19 19:50:47 +0200

    Update playable_test_arcing_projectile_weapon_1_component_bullet.gd
    I thought the splash test only detected entities. I changed the file again to print anything hit as it was doing before.

--- entities/playables/_shared/components/weapons/test_arcing_projectile_weapon_1/bullet/playable_test_arcing_projectile_weapon_1_component_bullet.gd
+++ entities/playables/_shared/components/weapons/test_arcing_projectile_weapon_1/bullet/playable_test_arcing_projectile_weapon_1_component_bullet.gd
@@ -12,13 +12,9 @@ func _on_collided(collision: KinematicCollision3D) -> void:
 	_splash_test.force_shapecast_update();
 
 	for collision_index: int in range(_splash_test.get_collision_count()):
-		var collider: Object = _splash_test.get_collider(collision_index);
+		var splash_collider: Object = _splash_test.get_collider(collision_index);
 
-		# The splash test only masks entities but let's add a check just in case.
-		if not EntityComponent.is_object_an_entity(collider):
-			continue;
-
-		print("Splash detected this entity: ", collider);
+		print("Splash Detected: ", splash_collider);
 
 
 func _on_life_duration_timer_timeout() -> void:
